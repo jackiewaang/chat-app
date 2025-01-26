0a1e13d5fa513ea03aa70c72823979f4815195b1
jackiewaang <zjackiewang03@gmail.com>
2025-01-26 22:08:42 +0000

    designed homepage layout

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,9 +1,10 @@
+import { HERO_HEADING, HERO_SUBHEADING1, HERO_SUBHEADING2 } from "../constants"
 
 const Homepage = () => {
 
   return (
     <>
-    <div className="bg-fixed bg-cover h-screen">
+    <div className="bg-gray-100 bg-[url(./assets/chatIcon.svg)] bg-repeat bg-center h-screen flex">
         <nav className="bg-white shadow-md fixed w-full z-10">
           <div className="flex justify-center md:justify-between items-center p-4">
             <h1 className="text-3xl font-bold">DotChat</h1>
@@ -18,6 +19,17 @@ const Homepage = () => {
             </div>
           </div>
         </nav>
+        <div className="w-full pt-20 flex justify-center items-center">
+          <div className="border border-black rounded-lg bg-white shadow-lg p-10 flex flex-col justify-center items-center gap-5">
+            <h1 className="text-6xl font-extrabold">{HERO_HEADING}</h1>
+            <p className="text-xl">{HERO_SUBHEADING1}</p>
+            <p className="text-xl">{HERO_SUBHEADING2}</p>
+            <div className="flex items-center justify-center gap-5">
+              <a href="/signup" className="text-lg border border-black hover:bg-black hover:text-white py-2 px-4 rounded-lg transition duration-300">Get Started</a>
+              <a href="/features" className="text-lg bg-black text-white hover:bg-gray-700 py-2 px-4 rounded-lg transition duration-300">Learn More</a>
+            </div>
+          </div>
+        </div>
         </div>
     </>
   )
---
+++ client/src/constants/index.js
@@ -0,0 +1,3 @@
+export const HERO_HEADING = "Stay Connected, Effortlessly";
+export const HERO_SUBHEADING1 = "DotChat is your all-in-one platform for real-time communication.";
+export const HERO_SUBHEADING2 = "Whether you're catching up with friends or collaborating with teammates."
